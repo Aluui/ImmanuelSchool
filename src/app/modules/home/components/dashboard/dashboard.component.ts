fc719a6c72bc06b19df1b44a834d72833d4f4c51
Chibuzor Sam Alumba <sama@edisonproperties.ca>
2019-11-21 00:12:18 -0600

    added a carousel

--- src/app/modules/home/components/dashboard/dashboard.component.ts
+++ src/app/modules/home/components/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SafeStyle } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  slides: { image: string }[];
+  constructor() {}
 
   ngOnInit() {
+    this.slides = [
+      { image: "./../../../../../assets/images/landscape.jpeg" },
+      { image: "./../../../../../assets/images/landscape2.jpeg" }
+    ];
   }
-
 }
